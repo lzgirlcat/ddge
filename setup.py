8652c650dfc8621ed2f2f4af9a9ccf2cee03dc29
lz <lz@girlc.at>
2023-02-27 17:58:01 +0100

    fix version bc im a dumbo!!

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def parse_requirements_file(path):
 
 setuptools.setup(
     name="ddge",
-    version='0.2.137.a',
+    version='0.2.137.1',
     description="DuckDuckGo Email Protection CLI",
     long_description=long_description(),
     long_description_content_type="text/markdown",
